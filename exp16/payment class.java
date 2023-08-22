9313a45b5411b6dc49a53b511234c2f60d84e4cc
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-22 21:49:04 +0530

    Add files via upload

---
+++ exp16/payment class.java
@@ -0,0 +1,68 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class payment class {
+
+    /**
+     * Default constructor
+     */
+    public payment class() {
+    }
+
+    /**
+     * 
+     */
+    public int payment_id;
+
+    /**
+     * 
+     */
+    public int payment_customer_id;
+
+    /**
+     * 
+     */
+    public date payment_amount;
+
+    /**
+     * 
+     */
+    public string paymen;
+
+    /**
+     * 
+     */
+    public ;string payment_description;
+
+    /**
+     * 
+     */
+    public void addpayment() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void editpayment() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void deletepayment() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void searchpayment() {
+        // TODO implement here
+    }
+
+}
