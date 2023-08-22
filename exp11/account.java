16e11f48c351873568ce3546d4fa034f85b2efe8
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-22 12:21:32 +0530

    Add files via upload

---
+++ exp11/account.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class account {
+
+    /**
+     * Default constructor
+     */
+    public account() {
+    }
+
+    /**
+     * 
+     */
+    public integer accountno;
+
+    /**
+     * 
+     */
+    public integer balance;
+
+    /**
+     * 
+     */
+    public integer due;
+
+    /**
+     * 
+     */
+    public void accounting() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void accountpay() {
+        // TODO implement here
+    }
+
+}
---
+++ exp11/administrator.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class administrator {
+
+    /**
+     * Default constructor
+     */
+    public administrator() {
+    }
+
+    /**
+     * 
+     */
+    public string name;
+
+    /**
+     * 
+     */
+    public string id;
+
+    /**
+     * 
+     */
+    public void bill 1() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void bill 2() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void traceorder() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void pay bill1() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void pay bill2() {
+        // TODO implement here
+    }
+
+}
