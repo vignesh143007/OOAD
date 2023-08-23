05090ef3a60ca351e8c0e3bb5d17152809b79178
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-23 13:21:47 +0530

    Add files via upload

---
+++ exp30/Invoice.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Invoice {
+
+    /**
+     * Default constructor
+     */
+    public Invoice() {
+    }
+
+    /**
+     * 
+     */
+    private items arraylist;
+
+    /**
+     * 
+     */
+    private double bill;
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation2() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation3() {
+        // TODO implement here
+    }
+
+}
---
+++ exp30/permission class.java
@@ -0,0 +1,68 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class permission class {
+
+    /**
+     * Default constructor
+     */
+    public permission class() {
+    }
+
+    /**
+     * 
+     */
+    public int permission_id;
+
+    /**
+     * 
+     */
+    public int permission_role_id;
+
+    /**
+     * 
+     */
+    public string permission_title;
+
+    /**
+     * 
+     */
+    public string permission_module;
+
+    /**
+     * 
+     */
+    public string permission_description;
+
+    /**
+     * 
+     */
+    public void addpermission() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void editpermission() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void deletepermission() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void searchpermission() {
+        // TODO implement here
+    }
+
+}
