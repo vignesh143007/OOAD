496c55f59fae642b61f63796e36eeb4fa89f1ab1
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-23 19:38:18 +0530

    Add files via upload

---
+++ exp38/Salary Details.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Salary Details extends HR {
+
+    /**
+     * Default constructor
+     */
+    public Salary Details() {
+    }
+
+    /**
+     * 
+     */
+    public void create bank Account to deposit salary;
+
+    /**
+     * 
+     */
+    public void salary() {
+        // TODO implement here
+    }
+
+}
