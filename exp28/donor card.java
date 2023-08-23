dcf47522df37baa71afcc34cd1cbe547b10cf894
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-23 12:46:26 +0530

    Add files via upload

---
+++ exp28/donor card.java
@@ -0,0 +1,49 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class donor card {
+
+    /**
+     * Default constructor
+     */
+    public donor card() {
+    }
+
+    /**
+     * 
+     */
+    public int id;
+
+    /**
+     * 
+     */
+    public string owner name;
+
+    /**
+     * 
+     */
+    public date donationdate;
+
+    /**
+     * 
+     */
+    public string details;
+
+    /**
+     * 
+     */
+    public void add() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void verify() {
+        // TODO implement here
+    }
+
+}
