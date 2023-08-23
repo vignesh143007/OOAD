c050f6fb903d992794c1cdec2cde3348015d3ba0
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-23 12:42:37 +0530

    Add files via upload

---
+++ exp27/Blood Type.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Blood Type {
+
+    /**
+     * Default constructor
+     */
+    public Blood Type() {
+    }
+
+    /**
+     * 
+     */
+    public int id;
+
+    /**
+     * 
+     */
+    public string blood type;
+
+}
---
+++ exp27/donor.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class donor {
+
+    /**
+     * Default constructor
+     */
+    public donor() {
+    }
+
+    /**
+     * 
+     */
+    public int id;
+
+    /**
+     * 
+     */
+    public string name;
+
+    /**
+     * 
+     */
+    public string status;
+
+    /**
+     * 
+     */
+    public string bloodtype;
+
+    /**
+     * 
+     */
+    public int donorcard;
+
+    /**
+     * 
+     */
+    public void add() {
+        // TODO implement here
+    }
+
+}
