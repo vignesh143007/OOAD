ec4b42a03b938760cf58af77625329fb7c671183
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-23 09:00:47 +0530

    Add files via upload

---
+++ exp17/company.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class company {
+
+    /**
+     * Default constructor
+     */
+    public company() {
+    }
+
+    /**
+     * 
+     */
+    public void street address;
+
+    /**
+     * 
+     */
+    public void city;
+
+    /**
+     * 
+     */
+    public void state;
+
+    /**
+     * 
+     */
+    public void zip;
+
+    /**
+     * 
+     */
+    public void phone;
+
+}
