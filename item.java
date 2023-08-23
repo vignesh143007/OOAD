c55125cce55c7375f08a59ef1e806bb56f14865b
vignesh143007 <137189542+vignesh143007@users.noreply.github.com>
2023-08-23 19:36:25 +0530

    Add files via upload

---
+++ item.java
@@ -0,0 +1,49 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class item {
+
+    /**
+     * Default constructor
+     */
+    public item() {
+    }
+
+    /**
+     * 
+     */
+    public string name;
+
+    /**
+     * 
+     */
+    public integer id;
+
+    /**
+     * 
+     */
+    public integer rate;
+
+    /**
+     * 
+     */
+    public integer available;
+
+    /**
+     * 
+     */
+    public void getperffume() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void getdolls() {
+        // TODO implement here
+    }
+
+}
---
+++ trader_vkr.java
@@ -0,0 +1,65 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class trader_vkr {
+
+    /**
+     * Default constructor
+     */
+    public trader_vkr() {
+    }
+
+    /**
+     * 
+     */
+    public string username;
+
+    /**
+     * 
+     */
+    public string password;
+
+    /**
+     * 
+     */
+    public integer accountno;
+
+    /**
+     * 
+     */
+    public void login() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void sourcing() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void place order() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void play() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void layout() {
+        // TODO implement here
+    }
+
+}
